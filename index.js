1120618e91584920789d3691e5efad479ed9a390
ahm3edmoham3d <ahmed.mohamed20768@gmail.com>
2024-08-19 19:39:04 +0300

    Post Request

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const app = express();
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+// middleware
+app.use(express.json());
+
 // get req
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Get Request" });
+});
+
+// post req
+app.post("/", (req, res) => {
+  res.status(201).json(req.body);
 });
