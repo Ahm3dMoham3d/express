a3857c58d79bce615bdfa3b47ef7f892f688c524
ahm3edmoham3d <ahmed.mohamed20768@gmail.com>
2024-08-19 19:16:31 +0300

    Get Request

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const app = express();
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+// get req
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Get Request" });
+});
